7116178524dc5204a4556ab55f1052610a7c27dd
aihuxi <worongbo@163.com>
2018-10-02 16:46:34 +0800

    a

---
+++ src/main/java/com/rong/NettystudyApplication.java
@@ -0,0 +1,13 @@
+package com.rong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettystudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettystudyApplication.class, args);
+		System.out.println("test1");
+	}
+}
